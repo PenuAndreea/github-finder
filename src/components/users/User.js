ac90d98067a4d9e927157153258ce32493a61719
Andreea <andreeapenu.work@gmail.com>
2019-06-10 16:42:07 +0200

    add bio section

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login)
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  }
+
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -20,7 +31,72 @@ export class User extends Component {
       hireable
     } = this.props.user
     const { loading } = this.props
-    return <div>{name}</div>
+    if (loading) {
+      return <Spinner />
+    } else {
+      return (
+        <Fragment>
+          <Link to='/' className='btn btn-light'>
+            Back to Search
+          </Link>
+          Hireable:{' '}
+          {hireable ? (
+            <i className='fas fa-check text-success' />
+          ) : (
+            <i className='fas fa-times-circle text-danger' />
+          )}
+          <div className='card grid-2'>
+            <div className='all-center'>
+              <img
+                src={avatar_url}
+                className='round-img'
+                alt=''
+                style={{ width: '150px' }}
+              />
+              <h1>{name}</h1>
+              <p>Location: {location}</p>
+            </div>
+            <div>
+              {bio && (
+                <Fragment>
+                  <h3>Bio</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a href={html_url} className='btn btn-dark my-1'>
+                Visit Github Profile
+              </a>
+              <ul>
+                <li>
+                  {login && (
+                    <Fragment>
+                      <strong>Username: </strong>
+                      {login}
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {company && (
+                    <Fragment>
+                      <strong>Company: </strong>
+                      {company}
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {blog && (
+                    <Fragment>
+                      <strong>Website: </strong>
+                      {blog}
+                    </Fragment>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Fragment>
+      )
+    }
   }
 }
 
